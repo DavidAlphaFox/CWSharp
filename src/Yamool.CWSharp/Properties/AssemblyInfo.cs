23e831c96cc04f4fc1adc5f398dd9d8776ef4c34
zhengchun <zhengchunster@gmail.com>
2017-02-10 17:58:16 +0800

    upgrade to vs2017 RC

--- src/Yamool.CWSharp/Properties/AssemblyInfo.cs
+++ src/Yamool.CWSharp/Properties/AssemblyInfo.cs
@@ -1,19 +1,19 @@
 ﻿// Copyright (c) Yamool. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+//using System.Reflection;
+//using System.Runtime.CompilerServices;
+//using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Yamool.CWSharp.dll")]
-[assembly: AssemblyDescription("The chinese word segmentation library.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Yamool Inc.")]
-[assembly: AssemblyProduct("Yamool.CWSharp")]
-[assembly: AssemblyCopyright("Copyright © Yamool Inc. All rights reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.0.5666.1622")]
-[assembly: AssemblyInformationalVersion("1.0.5666.1622")]
+//[assembly: AssemblyTitle("Yamool.CWSharp.dll")]
+//[assembly: AssemblyDescription("The chinese word segmentation library.")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("Yamool Inc.")]
+//[assembly: AssemblyProduct("Yamool.CWSharp")]
+//[assembly: AssemblyCopyright("Copyright © Yamool Inc. All rights reserved.")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+//[assembly: ComVisible(false)]
+//[assembly: AssemblyVersion("1.0")]
+//[assembly: AssemblyFileVersion("1.0.5666.1622")]
+//[assembly: AssemblyInformationalVersion("1.0.5666.1622")]
